b0ec43bf0016187dc813f6e8e10e3fae48b2f9df
Tarun D <89535221+Klavious@users.noreply.github.com>
2025-04-27 00:50:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Simple in-memory user storage
+const users = [];
+
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  
+  const userExists = users.find(u => u.username === username);
+  if (userExists) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  users.push({ username, password });
+  return res.json({ message: 'Signup successful!' });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  
+  const user = users.find(u => u.username === username && u.password === password);
+  if (user) {
+    return res.json({ message: 'Login successful!' });
+  } else {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
